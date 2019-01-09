0905268709a1157d615507c6d01ad81e2f4649dc
umangg21 <umangg21@gmail.com>
2019-01-09 20:14:51 +0530

    Updates

---
+++ precache-manifest.4b93d3208cbfc094547f2ddcbf31c523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffc437641b127d50a47b277d4ea0da5b",
+    "url": "/trello-mock/static/media/background.ffc43764.jpg"
+  },
+  {
+    "revision": "24ff89874c6fad77caeb",
+    "url": "/trello-mock/static/js/runtime~main.24ff8987.js"
+  },
+  {
+    "revision": "30c8d9e4d842a8cfc15c",
+    "url": "/trello-mock/static/js/main.30c8d9e4.chunk.js"
+  },
+  {
+    "revision": "7c96527fa42d03269d69",
+    "url": "/trello-mock/static/js/1.7c96527f.chunk.js"
+  },
+  {
+    "revision": "30c8d9e4d842a8cfc15c",
+    "url": "/trello-mock/static/css/main.6bd65ece.chunk.css"
+  },
+  {
+    "revision": "1479dbf0214926e665144c31bd6567e4",
+    "url": "/trello-mock/index.html"
+  }
+];
